eb621fd2983634292cfb96fbac15a65eb9f5f340
Peter N Jenkin <peternjenkin@gmail.com>
2019-02-07 14:15:43 +0000

    JSON served from Express

--- server.js
+++ server.js
@@ -6,7 +6,16 @@ var app = express();    // call express to start with
 
 app.get('/',(request, response) =>
 {   // request and response
-  response.send('<h1>Hello from Express PNJ</h1>');
+  // response.send('<h1>Hello from Express PNJ</h1>');
+  response.send(
+  {
+    name: 'P.N.J.',
+    likes:
+    [
+      'reading',
+      'walking'
+    ]
+  })
 }); // url and function to run in this case/route
 
 app.listen(3000);   // listen on port 3000
